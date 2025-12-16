27dadbbb66f9a12020b8b46a5ef06278d9991928
Samiya mahmud <samiyamahmud373@gmail.com>
2025-12-16 01:47:05 -0800

    Add files via upload
    my first js color coding project

---
+++ index.js
@@ -0,0 +1,43 @@
+// let x="samu";
+// let y="ti";
+// let z=x+y;
+// console.log(z);
+
+// // document.getElementsByTagName("h1")
+// console.log('${x} ${y} ${z}');
+
+// const ab=6;
+// if(ab>0){
+//     console.log("true");
+// }else{
+//     console.log("false");
+// }
+// function print(){
+//     console.log("my nmae is samiya");
+// }
+// print();
+// function call(a,b){
+//     const diff=a-b;
+//     return diff;
+// }
+// var a=call(6,2);
+// console.log("diff= "+ a);
+ 
+
+// const fun=function(c,d){
+//     const mul=c*d;
+//     return mul;
+// }d
+// var co=fun(4,5);
+// console.log(co);
+// // arrow ;function
+// const vari=(e,r)=>{
+//     const div=e/r;
+//     return div;
+// }
+// var r=vari(9,3);
+// console.log(r);
+function fun(){
+    document.getElementById("h1").innerHTML("buy");
+}
+fun();
